4d86e5717838f840401633c158294a2f5bdabdca
Auca <155678368+Auc4@users.noreply.github.com>
2026-09-11 22:33:12 +0000

    demostración

--- test_presupuesto.py
+++ test_presupuesto.py
@@ -2,7 +2,7 @@ from presupuesto_analisis import calcular_presupuesto
 
 # 1. CASO FELIZ (Happy Path)
 def test_calculo_normal():
-    intereses, total, cuota = calcular_presupuesto(1000.0, 2, 2)
+    intereses, total, cuota = calcular_presupuesto(1000.0, 0, 2)
     # 1000 * 0.02 * (2**2) = 80.0
     assert intereses == 80.0
     assert total == 1080.0
